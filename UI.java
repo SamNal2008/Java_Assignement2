b2c9707292bf6f2d507ff591232fcc49b435e2b0
evan hays <evan.hays@epita.fr>
2019-06-29 02:19:12 +0800

    vahicle

--- UI.java
+++ UI.java
@@ -1,36 +1,42 @@
 
 /**
-* Décrivez votre classe UI ici.
-*
-* @author (votre nom)
-* @version (un numéro de version ou une date)
-*/
+ * Décrivez votre classe UI ici.
+ *
+ * @author (votre nom)
+ * @version (un numéro de version ou une date)
+ */
 public class UI
 {
-public static void main(String[] args) 
-{
-int t22[][] = {{1,2,3},
-               {4,5,6},
-               {7,8,9}};
-display(t22);
-}
-
-public static void display_separation(int n)
-{
-for (int i = 0; i < n;i++)
-{
-    System.out.print("+---");
-}
-System.out.println("+");
-}
-
-public static void display(int tab[][])
-{
-    for(int i = 0; i < tab.length; i++)
+    public static void main(String[] args) 
     {
-        display_separation(tab[i].length);
-        for (int j = 0; j < tab[i].length;j++)
+        System.out.println("Bienvenido en el cartel de la coche y de la vane");
+        Vehicle vec1 = new Car(12345, "aveo" , 2016,  "diesel",7);
+        Vehicle vec2 = new Car(12346, "mini" , 2014,  "gasual",5);
+        Vehicle vec3 = new Car(12347, "picaso" , 2010,  "oil",5);
+        Vehicle vec4 = new Van(12348, "combi" , 2004,  "gasual",250);
+        vec1.display();
+        vec2.display();
+        vec3.display();
+        vec4.display();
+
+    }
+
+    public static void display_separation(int n)
+    {
+        for (int i = 0; i < n;i++)
         {
+            System.out.print("+---");
+        }
+        System.out.println("+");
+    }
+
+    public static void display(int tab[][])
+    {
+        for(int i = 0; i < tab.length; i++)
+        {
+            display_separation(tab[i].length);
+            for (int j = 0; j < tab[i].length;j++)
+            {
                 System.out.format("|%2d ",tab[i][j]);
             }
             System.out.println("|");
--- Vehicle.java
+++ Vehicle.java
@@ -8,46 +8,96 @@ import java.util.*;
 public class Vehicle
 {
     // instance variables - replace the example below with your own
-    private int RN;//registration number
-    private String modelName;
-    private int year;//year of registration
+    protected int RN;//registration number
+    protected String modelName;
+    protected int year;//year of registration
+    protected String TypeFuel;
+    protected boolean repair;
 
     /**
      * Constructor for objects of class Vehicle
      */
     public Vehicle()
     {
-        
+        this.RN = 0;
+        this.modelName = "";
+        this.year = 0;
+        this.TypeFuel = "";
+        this.repair = true;
     }
-    
+
+    public Vehicle(int RN, String model , int year,  String fuel)
+    {
+        this.RN = RN;
+        this.modelName = model;
+        this.year = year;
+        this.TypeFuel = fuel;
+    }
+
     public void setRN(int RN) //set registration number
     {
         this.RN = RN;
     }
-    
+
     public int getRN() //get registration number
     {
         return this.RN;
     }
-    
+
     public void setmodel(String modelName)
     {
         this.modelName = modelName;
     }
-    
+
     public String getModel()
     {
         return this.modelName;
     }
-    
+
     public void setYear(int year)
     {
         this.year = year;
     }
-    
+
     public int getYear()
     {
         return this.year;
+    }
+
+    public String getFuel()
+    {
+        return this.TypeFuel;
+    }
+
+    public void setFuel(String Fuel)
+    {
+        this.TypeFuel = Fuel;
+    }
+
+    public boolean getRepair()
+    {
+        return this.repair;
+    }
+
+    public void setRepair(boolean repair)
+    {
+        this.repair = repair;
+    }
+
+    public void display()
+    {
+        System.out.format("|%5d",this.RN);
+        System.out.format("|%10s",this.modelName);
+        System.out.format("|%4d" , this.year);
+        System.out.format("|%10s",this.TypeFuel);
+        if (this.repair)
+        {
+            System.out.print("|   Yes");
+        }
+        else
+        {
+            System.out.print("|    No");
+        }
     }
 
 }
